1a422359b82abfe86d6f378704776acee4943806
hossainahmed359 <hossainahmed359@gmail.com>
2021-10-29 18:09:54 +0600

    user create and finding via email functionality added

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const app = express();
 
+// Require 
+const ObjectId = require('mongodb').ObjectId;
+const { query } = require('express');
+require('dotenv').config();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 // Test
 app.get('/', (req, res) => {
@@ -27,9 +30,44 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // CRUD Operations
 async function run() {
     try {
+        // Get Database 
         await client.connect();
         const database = client.db("Traveller");
         const servicesCollection = database.collection("services");
+        const usersCollection = database.collection('users');
+
+        // Get Services 
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // Get Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        });
+
+        // Find User
+        app.get('/finduser/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            console.log(query)
+            const user = await usersCollection.findOne(query);
+            res.send(user)
+            console.log(user)
+        });
+
+        // Create User
+        app.post('/user', async (req, res) => {
+            const user = req.body.user;
+            const result = await usersCollection.insertOne(user)
+            console.log(user)
+            res.send(result)
+        })
 
     } finally {
         //   await client.close();
